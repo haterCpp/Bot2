ef1a5993d7d9dc7752101d74613fffa325908d1b
haterCpp <121799732+haterCpp@users.noreply.github.com>
2024-02-20 11:16:03 +0200

    Update MyBot.java
    working if whos write "crypto","crypto"

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import net.thauvin.erik.crypto.CryptoException;
 import net.thauvin.erik.crypto.CryptoPrice;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
 
 public class MyBot extends TelegramLongPollingBot {
     public MyBot() {
@@ -14,30 +17,55 @@ public class MyBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         var chatId = update.getMessage().getChatId();
-        var text =update.getMessage().getText();
-        try {
-        var message = new SendMessage();
-if(text.equals("/start")){
-    message.setText("Hello!");
-}else if (text.equals("btc")) {
-var price= CryptoPrice.spotPrice("BTC");
-    message.setText("BTC price: " + price.getAmount().doubleValue());
-}else if (text.equals("eth")) {
-    var price= CryptoPrice.spotPrice("ETH");
-    message.setText("ETH price: " + price.getAmount().doubleValue());
-}else if (text.equals("doge")) {
-    var price= CryptoPrice.spotPrice("Doge");
-    message.setText("DogeCoin price: " + price.getAmount().doubleValue());
-}else {
-    message.setText("Unknown command!");
-}
-        message.setChatId(chatId);
+        var text = update.getMessage().getText();
 
-            execute(message);
-        } catch (Exception e){
-            System.out.println("Error");
+        try {
+            if (text.equals("/start")) {
+                sendMessage(chatId, "Hello!");
+            } else if (text.contains(",")) {
+                String[] cryptocurrencies = text.split(",");
+                StringBuilder messageBuilder = new StringBuilder();
+                for (String crypto : cryptocurrencies) {
+                    messageBuilder.append(getPriceMessage(crypto.trim())).append(", ");
+                }
+                String message = messageBuilder.toString().replaceAll(", $", "");
+                sendMessage(chatId, message);
+            } else if (text.equals("btc")) {
+                sendPrice(chatId, "BTC");
+            } else if (text.equals("eth")) {
+                sendPrice(chatId, "ETH");
+            } else if (text.equals("doge")) {
+                sendPrice(chatId, "Doge");
+            } else if (text.equals("all")) {
+                sendPrice(chatId, "BTC");
+                sendPrice(chatId, "ETH");
+                sendPrice(chatId, "Doge");
+            } else {
+                sendMessage(chatId, "Unknown command!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error!");
         }
     }
+
+    String getPriceMessage(String name) throws Exception {
+        var price = CryptoPrice.spotPrice(name);
+        return name + " price: " + price.getAmount().doubleValue();
+    }
+
+    void sendPrice(long chatId, String name) throws Exception {
+        var priceMessage = getPriceMessage(name);
+        sendMessage(chatId, priceMessage);
+    }
+
+    void sendMessage(long chatId, String text) throws Exception {
+        var message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        execute(message);
+    }
+
+
     @Override
     public String getBotUsername() {
         return "Crying_java_4_bot";
